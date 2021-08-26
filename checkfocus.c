56b0f4c1e43d164cbe950f466e109b61ac35db02
Graeme Vetterlein <graeme.git@vetterlein.com>
2021-08-26 21:07:52 +0100

    Added some GPL decoration, some Autotools included files still missing

--- checkfocus.c
+++ checkfocus.c
@@ -2,6 +2,24 @@
 #include <getopt.h>
 #include <string.h>
 
+/*
+    checkfocus - report statistics on a jpeg level of focus
+    Copyright (C) 2021  Graeme Vetterlein
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 /* defined by jmorecfg.h ... */
 //#define RGB_RED         0       /* Offset of Red in an RGB scanline element */
 //#define RGB_GREEN       1       /* Offset of Green */
