b87f783ed782887fbeee632c294074a421095eb0
Graeme Vetterlein <graeme.git@vetterlein.com>
2021-08-26 16:21:42 +0100

    tidy up of documentation

--- SETUP_4_AUTOMAKE.sh
+++ SETUP_4_AUTOMAKE.sh
@@ -4,6 +4,10 @@ cat > Makefile.am <<EOF
 bin_PROGRAMS=checkfocus bestfocus
 checkfocus_SOURCES = checkfocus.c cf_util.c
 bestfocus_SOURCES  = bestfocus.c  cf_util.c
+man1_MANS = doc/checkfocus.man doc/bestfocus.man 
+dist_man_MANS = doc/checkfocus.man doc/bestfocus.man
+bestfocus_CFLAGS=-Wconversion
+checkfocus_CFLAGS=-Wconversion
 EOF
 
 touch NEWS README AUTHORS ChangeLog
